3e524ca005fe77c5bb3190a6a48f1bde516f3526
Dino Saw <dino.saw@bugzstudio.com>
2025-09-04 17:15:33 +0800

    feat: Android devices that support Edge-to-Edge will use thicker toolbar height

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -119,7 +119,7 @@ public class InAppBrowser extends CordovaPlugin {
     private static final String BEFORELOAD = "beforeload";
     private static final String FULLSCREEN = "fullscreen";
 
-    private static final int TOOLBAR_HEIGHT = 48;
+    private static final int TOOLBAR_HEIGHT = Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q ? 72 : 48;
 
     private static final List customizableOptions = Arrays.asList(CLOSE_BUTTON_CAPTION, TOOLBAR_COLOR, NAVIGATION_COLOR, CLOSE_BUTTON_COLOR, FOOTER_COLOR);
 
